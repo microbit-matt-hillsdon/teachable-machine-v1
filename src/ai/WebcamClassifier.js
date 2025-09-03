27e71eab2cbd8ef511f661e95cf113c03a9337c1
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-09-03 15:08:57 +0100

    Fix missing import

--- src/ai/WebcamClassifier.js
+++ src/ai/WebcamClassifier.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+import GLOBALS from '../config.js';
 
 const IMAGE_SIZE = 227;
 const INPUT_SIZE = 1000;
