e16831c26fece1b16c637a725da3fb98c435cb57
Grace <grace@microbit.org>
2025-10-02 14:06:40 +0100

    Setup initial MakeCode project

--- src/outputs/CodeEditor.js
+++ src/outputs/CodeEditor.js
@@ -60,13 +60,14 @@ class CodeEditor {
             {
                 controllerId: "Teachable machine with micro:bit",
                 queryParams: { hideLanguage: "1" },
-                initialProjects: async () => [defaultMakeCodeProject],
+                initialProjects: async () => [initialMakeCodeProject],
                 onEditorContentLoaded: (e) =>
                     console.log("MakeCode is now ready"),
                 onWorkspaceSave: (e) => {
                     console.log(e.project.header.id, e.project);
                 },
                 onDownload: this.onDownload.bind(this),
+                onBack: this.close.bind(this),
             },
             () => this.iframe
         );
@@ -100,14 +101,42 @@ class CodeEditor {
     }
 }
 
-const defaultMakeCodeProject = {
+const extensionVersion = "5e046c731a54cbfc41f7d8b63300ffa020b3060e";
+const initialMakeCodeProject = {
+    header: {
+        target: "microbit",
+        targetVersion: "8.0.16",
+        editor: "blocksprj",
+        name: "TMv1Integration",
+        meta: {},
+        pubId: "",
+        pubCurrent: false,
+        id: "ad5c181a-e6d9-4f62-8e18-4fd34b7617f6",
+        recentUse: 1759408610,
+        modificationTime: 1759408610,
+        path: "TMv1Integration",
+        cloudCurrent: false,
+        saveId: null,
+        githubCurrent: false,
+    },
     text: {
+        "pxt.json": JSON.stringify({
+            name: "TMv1Integration",
+            description: "",
+            dependencies: {
+                core: "*",
+                microphone: "*",
+                radio: "*", // Needed to compile.
+                "TM Extension": `github:microbit-grace/tm-extension#${extensionVersion}`,
+            },
+            files: ["main.ts", "main.blocks", "README.md"],
+            preferredEditor: "blocksprj",
+        }),
+        "README.md": "",
         "main.blocks":
-            '<xml xmlns="http://www.w3.org/1999/xhtml">\n  <block type="pxt-on-start" id=",{,HjW]u:lVGcDRS_Cu|" x="-247" y="113"></block>\n</xml>',
-        "main.ts": "",
-        "README.md": " ",
-        "pxt.json":
-            '{\n    "name": "Untitled",\n    "dependencies": {\n        "core": "*"\n , "radio": "*"\n   },\n    "description": "",\n    "files": [\n        "main.blocks",\n        "main.ts",\n        "README.md"\n    ]\n}',
+            '<xml xmlns="https://developers.google.com/blockly/xml"><variables></variables><block type="TMMachineLearning_onMLGreenStart" x="0" y="0"><statement name="HANDLER"><block type="music_playable_play"><field name="playbackMode">music.PlaybackMode.InBackground</field><value name="toPlay"><shadow type="soundExpression_builtinPlayableSoundEffect"><field name="soundExpression">soundExpression.giggle</field></shadow></value></block></statement></block><block type="TMMachineLearning_onMLPurpleStart" x="0" y="149"><statement name="HANDLER"><block type="music_playable_play"><field name="playbackMode">music.PlaybackMode.InBackground</field><value name="toPlay"><shadow type="soundExpression_builtinPlayableSoundEffect"><field name="soundExpression">soundExpression.happy</field></shadow></value></block></statement></block><block type="TMMachineLearning_onMLOrangeStart" x="-3" y="308"><statement name="HANDLER"><block type="music_playable_play"><field name="playbackMode">music.PlaybackMode.InBackground</field><value name="toPlay"><shadow type="soundExpression_builtinPlayableSoundEffect"><field name="soundExpression">soundExpression.hello</field></shadow></value></block></statement></block></xml>',
+        "main.ts":
+            "TMMachineLearning.onMLOrangeStart(function () {\n    music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.InBackground)\n})\nTMMachineLearning.onMLPurpleStart(function () {\n    music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.InBackground)\n})\nTMMachineLearning.onMLGreenStart(function () {\n    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.InBackground)\n})\n",
     },
 };
 
